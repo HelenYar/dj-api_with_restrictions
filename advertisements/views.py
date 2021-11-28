77dd37d6c5abf901deb1cb12e674ca89938788f2
HelenYar <helen.yarosh82@gmail.com>
2021-11-28 22:28:39 +0300

    hw_dj_7_v3

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,9 +1,9 @@
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
+from advertisements.Permissions import IsOwnerPermission
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
@@ -22,5 +22,5 @@ class AdvertisementViewSet(ModelViewSet):
         if self.action in ["create"]:
             return [IsAuthenticated()]
         elif self.action in ["update", "destroy", "partial_update"]:
-            return [IsAdminUser()]
+            return [IsOwnerPermission()]
         return []
